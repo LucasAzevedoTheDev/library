21ec17141c32111d74456fbe938036aea635b6a1
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-06-30 22:37:30 -0300

    add library array

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
+const theLibrary = [];
+
 function Book(name, author, pages, read) {
   if(!new.target) {
     throw Error("You must use the 'new' operator to call the constructor")
   };
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
